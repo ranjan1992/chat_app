c598326437a6bd0067025700e25fdbf979f1cdcd
Ranjan Verma <ranjanverma@Ranjans-Air.lan>
2025-03-23 18:49:22 +0530

    DataBase Setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,17 @@
 import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+
+import dotenv from "dotenv";
 
 const app = express();
 
-import authRoutes from "./routes/auth.route.js";
+dotenv.config();
+const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
 
-app.listen(5001, () => {
-  console.log(`Running at port 5001`);
+app.listen(PORT, () => {
+  console.log(`Running at port ${PORT}`);
+  connectDB();
 });
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -10,3 +10,6 @@ router.post("/login", login);
 router.post("/logout", logout);
 
 export default router;
+
+//W8flONN1pN8wyVwm;
+//mongodb+srv://vermaranjan62:<db_password>@cluster0.urr6t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
