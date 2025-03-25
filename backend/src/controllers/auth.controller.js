4bb1c0bbfb98cc5eca0760a5ef66a4066e55bc7c
Ranjan Verma <ranjanverma@Ranjans-Air.lan>
2025-03-25 18:59:24 +0530

    Profile Page

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -96,15 +96,16 @@ export const updateProfile = async (req, res) => {
     }
 
     const uploadResponse = await cloudinary.uploader.upload(profilePic);
-    const updatedUser = User.findByIdAndUpdate(
+    const updatedUser = await User.findByIdAndUpdate(
       userId,
       { profilePic: uploadResponse.secure_url },
       { new: true }
     );
+
     res.status(200).json(updatedUser);
   } catch (error) {
-    console.log("error in update profile: ", error);
-    res.status(500).json({ message: "Internal server errorrr" });
+    console.log("error in update profile:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
